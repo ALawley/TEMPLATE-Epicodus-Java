a0cba22eddf8aa5e99821f2ce44cbaec3c441a4b
Anna Lawley and Michael Schennum <ALawley1@gmail.com>
2016-02-15 11:00:20 -0800

    move integration tests to main AppTest.java file

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,17 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest {
-
-  @Test
-  public void method_input_result() {
-    // eventually put your testing code here
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
   }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
 }
