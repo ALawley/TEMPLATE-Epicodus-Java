5bac625c9ad2ed409d48204adfaa723c4c2ace92
Anna Lawley <ALawley1@gmail.com>
2016-02-18 11:40:32 -0800

    Import fluentlenium filter constructor for apptest

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,6 +3,7 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
